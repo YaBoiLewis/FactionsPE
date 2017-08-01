177001e0f4f7544c0db3638f42fe6d5b7bd8f06e
YaBoiLewis <lewis@creeperlewis.com>
2017-08-01 11:05:55 +0500

    Update PermSet.php

--- src/factions/command/PermSet.php
+++ src/factions/command/PermSet.php
@@ -47,7 +47,7 @@ class PermSet extends Command
         $this->addParameter((new FactionParameter("faction"))->setDefaultValue("me"));
     }
 
-    public function perform(CommandSender $sender, $label, array $args)
+    public function perform(CommandSender $sender, string $label, array $args) :bool
     {
 
         $member = Members::get($sender);
